405dc4e995584ff6ceb323501dd3599cab246916
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-06-12 17:22:15 +0400

    Login & Register added

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import { ForgotPasswordPage } from "./ForgotPasswordPage";
+import { LoginPage } from "./LoginPage";
+import { ResetPasswordPage } from "./ResetPasswordPage";
+import { SignupPage } from "./SignupPage";
+
+
+export {
+    SignupPage,
+    LoginPage,
+    ForgotPasswordPage,
+    ResetPasswordPage
+}
